3bc15c2159cb4f6232ac55995c88983713012e1a
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-09-22 20:02:40 +0500

    Payment service finished

--- src/test/java/uz/ccrew/paymentservice/PaymentServiceApplicationTests.java
+++ src/test/java/uz/ccrew/paymentservice/PaymentServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package uz.ccrew.paymentservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PaymentServiceApplicationTests {
 
     @Test
